934049f483326685f8c79518c35cfa208fd3c969
emrebayirli <iebayirli@gmail.com>
2020-11-06 22:51:40 +0300

    base fragment added

--- app/src/main/java/com/iebayirli/appcent/base/BaseFragment.kt
+++ app/src/main/java/com/iebayirli/appcent/base/BaseFragment.kt
@@ -1,2 +1,41 @@
 package com.iebayirli.appcent.base
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.databinding.library.baseAdapters.BR
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<VM : BaseViewModel, B : ViewDataBinding> : Fragment() {
+
+    @get:LayoutRes
+    protected abstract val layoutId: Int
+
+    lateinit var binding: B
+
+    abstract val viewModel: VM
+
+    abstract fun initializeUI (savedInstanceState: Bundle?)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater, layoutId, container, false)
+        binding.setVariable(BR.viewModel, viewModel)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initializeUI(savedInstanceState)
+    }
+
+}
